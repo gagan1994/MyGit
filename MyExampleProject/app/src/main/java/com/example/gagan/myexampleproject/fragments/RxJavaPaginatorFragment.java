8181f058a695945e92db7f1eec7a387cdb481334
Gagan <gagan.s.patil18@gmail.com>
2018-03-05 10:51:14 +0530

    added

---
+++ MyExampleProject/app/src/main/java/com/example/gagan/myexampleproject/fragments/RxJavaPaginatorFragment.java
@@ -0,0 +1,50 @@
+
+package com.example.gagan.myexampleproject.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.gagan.myexampleproject.R;
+import com.example.gagan.myexampleproject.fragments.BasePagerFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RxJavaPaginatorFragment extends BasePagerFragment {
+
+    public static final String TAG = "RxJavaPaginatorFragment";
+    @BindView(R.id.rv_list)
+    RecyclerView rv_list;
+
+    public RxJavaPaginatorFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_rx_java_paginator, container, false);
+        ButterKnife.bind(view);
+        return view;
+    }
+
+    @Override
+    public String getTitle() {
+        return "Rx java Paginator";
+    }
+
+    @Override
+    public String getCustomTag() {
+        return TAG;
+    }
+
+}
